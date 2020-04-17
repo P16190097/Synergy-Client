698afb5f77424fba2d65c77d6ea0f45e55eb50a0
Joshua Mayo <mayoze.jm@googlemail.com>
2020-04-17 21:47:49 +0100

    added navbar component to create and edit team routes

--- src/routes/editTeam.jsx
+++ src/routes/editTeam.jsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import { Container, Header, Input, TextArea, Button, Message, Form as SemanticForm, Dimmer, Loader } from 'semantic-ui-react';
 import { Formik, Field, ErrorMessage } from 'formik';
 import { EDIT_TEAM, GET_TEAM_FOR_EDIT, DELETE_TEAM } from '../gql/team';
+import Navbar from '../components/navbar';
 
 const EditTeam = ({ match: { params: { teamId } } }) => {
     const [errorMsg, setErrorMsg] = useState(null);
@@ -88,97 +89,100 @@ const EditTeam = ({ match: { params: { teamId } } }) => {
     const { getTeam: team } = data;
 
     return (
-        <Container text>
-            <Header as="h2" color="orange">Edit Team</Header>
-            <Formik
-                initialValues={{
-                    teamName: team.name,
-                    description: team.description,
-                }}
-                onSubmit={values => {
-                    setErrorMsg(null);
-                    console.log('sending');
-                    editTeam({
-                        variables: {
-                            teamId: teamIdInt,
-                            teamName: values.teamName,
-                            description: values.description,
-                        },
-                    });
-                }}
-                validate={values => {
-                    const errors = {};
+        <>
+            <Navbar />
+            <Container text>
+                <Header as="h2" color="orange">Edit Team</Header>
+                <Formik
+                    initialValues={{
+                        teamName: team.name,
+                        description: team.description,
+                    }}
+                    onSubmit={values => {
+                        setErrorMsg(null);
+                        console.log('sending');
+                        editTeam({
+                            variables: {
+                                teamId: teamIdInt,
+                                teamName: values.teamName,
+                                description: values.description,
+                            },
+                        });
+                    }}
+                    validate={values => {
+                        const errors = {};
 
-                    if (!values.teamName) {
-                        errors.teamName = 'Please enter a team name';
-                    }
+                        if (!values.teamName) {
+                            errors.teamName = 'Please enter a team name';
+                        }
 
-                    return errors;
-                }}
-            >
-                {({ setTouched, touched, isSubmitting, setFieldValue, errors, handleSubmit, values }) => (
-                    //values
-                    <SemanticForm
-                        onKeyDown={(e) => {
-                            if (e.key === 'Enter' && !isSubmitting) {
-                                handleSubmit();
-                            }
-                        }}
-                    >
-                        <SemanticForm.Field>
-                            <Field
-                                name="teamName"
-                                component={Input}
-                                onBlur={() => setTouched({ teamName: true })}
-                                onChange={(e) => setFieldValue('teamName', e.target.value)}
-                                placeholder="Team name here"
-                                value={values.teamName}
-                                fluid
-                                error={Boolean(errors.teamName && touched.teamName)}
-                                label="Team Name"
-                            />
-                            <ErrorMessage name="teamName">{(msg) => (<Message negative>{msg}</Message>)}</ErrorMessage>
-                        </SemanticForm.Field>
-                        <br />
-
-                        <SemanticForm.Field>
-                            <Field
-                                name="description"
-                                component={TextArea}
-                                onBlur={() => setTouched({ description: true })}
-                                onChange={(e) => setFieldValue('description', e.target.value)}
-                                placeholder="Team description here"
-                                value={values.description}
-                                rows={3}
-                                label="Description"
-                            />
-                            <ErrorMessage name="description">{(msg) => (<Message negative>{msg}</Message>)}</ErrorMessage>
-                        </SemanticForm.Field>
-                        <br />
-
-                        {errorMsg && (
-                            <Message error header="An Error has occured:" list={errorMsg} />
-                        )}
-
-                        {isSubmitting && (
-                            <span>sending data...</span>
-                        )}
-
-                        <Button type="submit" color="orange" disabled={isSubmitting} onClick={() => handleSubmit()}>Confirm</Button>
-                        <Button
-                            negative
-                            onClick={() => deleteTeam({
-                                variables: {
-                                    teamId: teamIdInt,
-                                },
-                            })}
+                        return errors;
+                    }}
+                >
+                    {({ setTouched, touched, isSubmitting, setFieldValue, errors, handleSubmit, values }) => (
+                        //values
+                        <SemanticForm
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter' && !isSubmitting) {
+                                    handleSubmit();
+                                }
+                            }}
                         >
-                            Delete Team
-                        </Button>
-                    </SemanticForm>
-                )}
-            </Formik>
-        </Container>
+                            <SemanticForm.Field>
+                                <Field
+                                    name="teamName"
+                                    component={Input}
+                                    onBlur={() => setTouched({ teamName: true })}
+                                    onChange={(e) => setFieldValue('teamName', e.target.value)}
+                                    placeholder="Team name here"
+                                    value={values.teamName}
+                                    fluid
+                                    error={Boolean(errors.teamName && touched.teamName)}
+                                    label="Team Name"
+                                />
+                                <ErrorMessage name="teamName">{(msg) => (<Message negative>{msg}</Message>)}</ErrorMessage>
+                            </SemanticForm.Field>
+                            <br />
+
+                            <SemanticForm.Field>
+                                <Field
+                                    name="description"
+                                    component={TextArea}
+                                    onBlur={() => setTouched({ description: true })}
+                                    onChange={(e) => setFieldValue('description', e.target.value)}
+                                    placeholder="Team description here"
+                                    value={values.description}
+                                    rows={3}
+                                    label="Description"
+                                />
+                                <ErrorMessage name="description">{(msg) => (<Message negative>{msg}</Message>)}</ErrorMessage>
+                            </SemanticForm.Field>
+                            <br />
+
+                            {errorMsg && (
+                                <Message error header="An Error has occured:" list={errorMsg} />
+                            )}
+
+                            {isSubmitting && (
+                                <span>sending data...</span>
+                            )}
+
+                            <Button type="submit" color="orange" disabled={isSubmitting} onClick={() => handleSubmit()}>Confirm</Button>
+                            <Button
+                                negative
+                                onClick={() => deleteTeam({
+                                    variables: {
+                                        teamId: teamIdInt,
+                                    },
+                                })}
+                            >
+                                Delete Team
+                            </Button>
+                        </SemanticForm>
+                    )}
+                </Formik>
+            </Container>
+        </>
     );
 };
 
